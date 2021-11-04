52604b9108fb4a44a307b9543ff66b7c80f3589c
Andres Medina <jmedinajura@gmail.com>
2021-11-04 00:52:17 -0500

    Inicio Api

---
+++ api/TablaActividades/context/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TablaActividades.context
+{
+    public class AppDbContext
+    {
+    }
+}
